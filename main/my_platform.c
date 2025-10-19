e6a9f137d868dd7d91ab5a6a0eb7452581da4836
Daniel Dakhno <dakhnod@gmail.com>
2025-10-19 16:22:50 +0200

    initial commit

---
+++ main/my_platform.c
@@ -0,0 +1,233 @@
+// Example file - Public Domain
+// Need help? https://tinyurl.com/bluepad32-help
+
+#include <string.h>
+
+#include <uni.h>
+
+#include <driver/ledc.h>
+
+#define LEDC_TIMER              LEDC_TIMER_0
+#define LEDC_MODE               LEDC_LOW_SPEED_MODE
+#define LEDC_OUTPUT_IO          (5) // Define the output GPIO
+#define LEDC_CHANNEL            LEDC_CHANNEL_0
+#define LEDC_DUTY_RES           LEDC_TIMER_16_BIT // Set duty resolution to 13 bits
+#define LEDC_DUTY               (2 ^ (LEDC_DUTY_RES - 1)) // Set duty to 50%. (2 ** 13) * 50% = 4096
+#define LEDC_FREQUENCY          (4000) // Frequency in Hertz. Set frequency at 4 kHz
+
+// Custom "instance"
+typedef struct my_platform_instance_s {
+    uni_gamepad_seat_t gamepad_seat;  // which "seat" is being used
+} my_platform_instance_t;
+
+// Declarations
+static void trigger_event_on_gamepad(uni_hid_device_t* d);
+static my_platform_instance_t* get_my_platform_instance(uni_hid_device_t* d);
+
+//
+// Platform Overrides
+//
+static void my_platform_init(int argc, const char** argv) {
+    ARG_UNUSED(argc);
+    ARG_UNUSED(argv);
+
+    logi("custom: init()\n");
+        // Prepare and then apply the LEDC PWM timer configuration
+    ledc_timer_config_t ledc_timer = {
+        .speed_mode       = LEDC_MODE,
+        .duty_resolution  = LEDC_DUTY_RES,
+        .timer_num        = LEDC_TIMER,
+        .freq_hz          = 1,  // Set output frequency at 4 kHz
+        .clk_cfg          = LEDC_REF_TICK
+    };
+    ESP_ERROR_CHECK(ledc_timer_config(&ledc_timer));
+
+    // Prepare and then apply the LEDC PWM channel configuration
+    ledc_channel_config_t ledc_channel = {
+        .speed_mode     = LEDC_MODE,
+        .channel        = LEDC_CHANNEL,
+        .timer_sel      = LEDC_TIMER,
+        .intr_type      = LEDC_INTR_DISABLE,
+        .gpio_num       = LEDC_OUTPUT_IO,
+        .duty           = LEDC_DUTY, // Set duty to 0%
+        .hpoint         = 0
+    };
+    ESP_ERROR_CHECK(ledc_channel_config(&ledc_channel));
+
+
+    return;
+    // Set duty to 50%
+    ESP_ERROR_CHECK(ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, LEDC_DUTY));
+    // Update duty to apply the new value
+    ESP_ERROR_CHECK(ledc_update_duty(LEDC_MODE, LEDC_CHANNEL));
+}
+
+static void my_platform_on_init_complete(void) {
+    logi("custom: on_init_complete()\n");
+
+    // Safe to call "unsafe" functions since they are called from BT thread
+
+    // Start scanning
+    uni_bt_start_scanning_and_autoconnect_unsafe();
+    uni_bt_allow_incoming_connections(true);
+
+    // Based on runtime condition, you can delete or list the stored BT keys.
+    if (1)
+        uni_bt_del_keys_unsafe();
+    else
+        uni_bt_list_keys_unsafe();
+}
+
+static uni_error_t my_platform_on_device_discovered(bd_addr_t addr, const char* name, uint16_t cod, uint8_t rssi) {
+    // You can filter discovered devices here.
+    // Just return any value different from UNI_ERROR_SUCCESS;
+    // @param addr: the Bluetooth address
+    // @param name: could be NULL, could be zero-length, or might contain the name.
+    // @param cod: Class of Device. See "uni_bt_defines.h" for possible values.
+    // @param rssi: Received Signal Strength Indicator (RSSI) measured in dBms. The higher (255) the better.
+
+    // As an example, if you want to filter out keyboards, do:
+    if (((cod & UNI_BT_COD_MINOR_MASK) & UNI_BT_COD_MINOR_KEYBOARD) == UNI_BT_COD_MINOR_KEYBOARD) {
+        logi("Ignoring keyboard\n");
+        return UNI_ERROR_IGNORE_DEVICE;
+    }
+
+    return UNI_ERROR_SUCCESS;
+}
+
+static void my_platform_on_device_connected(uni_hid_device_t* d) {
+    logi("custom: device connected: %p\n", d);
+}
+
+static void my_platform_on_device_disconnected(uni_hid_device_t* d) {
+    logi("custom: device disconnected: %p\n", d);
+}
+
+static uni_error_t my_platform_on_device_ready(uni_hid_device_t* d) {
+    logi("custom: device ready: %p\n", d);
+    my_platform_instance_t* ins = get_my_platform_instance(d);
+    ins->gamepad_seat = GAMEPAD_SEAT_A;
+
+    trigger_event_on_gamepad(d);
+    return UNI_ERROR_SUCCESS;
+}
+
+static void my_platform_on_controller_data(uni_hid_device_t* d, uni_controller_t* ctl) {
+    static uint8_t leds = 0;
+    static uint8_t enabled = true;
+    static uni_controller_t prev = {0};
+    uni_gamepad_t* gp;
+
+    // Optimization to avoid processing the previous data so that the console
+    // does not get spammed with a lot of logs, but remove it from your project.
+    if (memcmp(&prev, ctl, sizeof(*ctl)) == 0) {
+        return;
+    }
+    prev = *ctl;
+    // Print device Id before dumping gamepad.
+    // This could be very CPU intensive and might crash the ESP32.
+    // Remove these 2 lines in production code.
+    //    logi("(%p), id=%d, \n", d, uni_hid_device_get_idx_for_instance(d));
+    //    uni_controller_dump(ctl);
+
+    if(ctl->klass != UNI_CONTROLLER_CLASS_GAMEPAD) {
+        return;
+    }
+
+    gp = &ctl->gamepad;
+
+    static bool pwm_enabled = false;
+
+    if(gp->throttle == 0) {
+        if(!pwm_enabled) {
+            return;
+        }
+        ledc_stop(LEDC_MODE, LEDC_CHANNEL, 0);
+        pwm_enabled = false;
+        return;
+    }
+
+    // ledc_set_freq(LEDC_MODE, LEDC_TIMER, 10);
+    pwm_enabled = true;
+    logi("axis: %d\n", gp->throttle);
+}
+
+static const uni_property_t* my_platform_get_property(uni_property_idx_t idx) {
+    ARG_UNUSED(idx);
+    return NULL;
+}
+
+static void my_platform_on_oob_event(uni_platform_oob_event_t event, void* data) {
+    switch (event) {
+        case UNI_PLATFORM_OOB_GAMEPAD_SYSTEM_BUTTON: {
+            uni_hid_device_t* d = data;
+
+            if (d == NULL) {
+                loge("ERROR: my_platform_on_oob_event: Invalid NULL device\n");
+                return;
+            }
+            logi("custom: on_device_oob_event(): %d\n", event);
+
+            my_platform_instance_t* ins = get_my_platform_instance(d);
+            ins->gamepad_seat = ins->gamepad_seat == GAMEPAD_SEAT_A ? GAMEPAD_SEAT_B : GAMEPAD_SEAT_A;
+
+            trigger_event_on_gamepad(d);
+            break;
+        }
+
+        case UNI_PLATFORM_OOB_BLUETOOTH_ENABLED:
+            logi("custom: Bluetooth enabled: %d\n", (bool)(data));
+            break;
+
+        default:
+            logi("my_platform_on_oob_event: unsupported event: 0x%04x\n", event);
+            break;
+    }
+}
+
+//
+// Helpers
+//
+static my_platform_instance_t* get_my_platform_instance(uni_hid_device_t* d) {
+    return (my_platform_instance_t*)&d->platform_data[0];
+}
+
+static void trigger_event_on_gamepad(uni_hid_device_t* d) {
+    my_platform_instance_t* ins = get_my_platform_instance(d);
+
+    if (d->report_parser.play_dual_rumble != NULL) {
+        d->report_parser.play_dual_rumble(d, 0 /* delayed start ms */, 150 /* duration ms */, 128 /* weak magnitude */,
+                                          40 /* strong magnitude */);
+    }
+
+    if (d->report_parser.set_player_leds != NULL) {
+        d->report_parser.set_player_leds(d, ins->gamepad_seat);
+    }
+
+    if (d->report_parser.set_lightbar_color != NULL) {
+        uint8_t red = (ins->gamepad_seat & 0x01) ? 0xff : 0;
+        uint8_t green = (ins->gamepad_seat & 0x02) ? 0xff : 0;
+        uint8_t blue = (ins->gamepad_seat & 0x04) ? 0xff : 0;
+        d->report_parser.set_lightbar_color(d, red, green, blue);
+    }
+}
+
+//
+// Entry Point
+//
+struct uni_platform* get_my_platform(void) {
+    static struct uni_platform plat = {
+        .name = "custom",
+        .init = my_platform_init,
+        .on_init_complete = my_platform_on_init_complete,
+        .on_device_discovered = my_platform_on_device_discovered,
+        .on_device_connected = my_platform_on_device_connected,
+        .on_device_disconnected = my_platform_on_device_disconnected,
+        .on_device_ready = my_platform_on_device_ready,
+        .on_oob_event = my_platform_on_oob_event,
+        .on_controller_data = my_platform_on_controller_data,
+        .get_property = my_platform_get_property,
+    };
+
+    return &plat;
+}
